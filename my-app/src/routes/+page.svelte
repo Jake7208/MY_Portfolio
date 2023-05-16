1436477da9acfcd39c9bd284605e965b7a2b75b6
Jake7208 <jake7208@gmail.com>
2023-05-15 23:19:14 -0600

    Got svelte configured for Typed.js

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Typed from 'typed.js';
+	import { onMount } from 'svelte';
+
+	let typedElement;
+	onMount(() => {
+		const options = {
+			strings: [
+				"Hello I'm Jacob",
+				"I'm A full stack developer with a focus in backend",
+				'Want to learn more about me?'
+			],
+			typeSpeed: 50,
+			backSpeed: 50,
+			loop: true
+		};
+		const typed = new Typed(typedElement, options);
+	});
+</script>
+
+<div class="text-3xl">
+	<span bind:this={typedElement} />
+</div>
